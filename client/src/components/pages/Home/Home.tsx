0c1dbd1c64af42d5b92a890ab9ad0f66944f0c1b
CCHASLES <clementine.chasles.ext@allianz-trade.com>
2025-02-28 22:20:27 +0100

    feature: list of users

--- client/src/components/pages/Home/Home.tsx
+++ client/src/components/pages/Home/Home.tsx
@@ -1,3 +1,91 @@
+import { useEffect, useState, useTransition } from 'react';
+import {
+  Alert,
+  CircularProgress,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from '@mui/material';
+
+interface User {
+  id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+}
+
 export const Home = () => {
-  return <div>Home</div>;
+  const [users, setUsers] = useState([]);
+  const [error, setError] = useState(false);
+  const [isPending, startTransition] = useTransition();
+  const loadUsers = async () => {
+    startTransition(async () => {
+      setError(false);
+      try {
+        const response = await fetch('/api/users', {
+          method: 'GET',
+        });
+        if (response.ok) {
+          const listOfUsers = await response.json();
+          setUsers(listOfUsers);
+        } else {
+          setError(true);
+        }
+      } catch {
+        setError(true);
+      }
+    });
+  };
+
+  useEffect(() => {
+    loadUsers();
+  }, []);
+  if (error) {
+    return (
+      <Alert className="m-8" severity="error">
+        Something went wrong while fetching the users. Please try again later.
+      </Alert>
+    );
+  }
+  if (isPending) {
+    return (
+      <div className="flex justify-center items-center mt-8">
+        <CircularProgress />
+      </div>
+    );
+  }
+  return (
+    <div className="flex items-start justify-start p-8">
+      <div className="w-full">
+        <Typography variant="h3" gutterBottom>
+          Users
+        </Typography>
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>First name</TableCell>
+                <TableCell>Last name</TableCell>
+                <TableCell>Email</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {users.map((user: User) => (
+                <TableRow key={user.id} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                  <TableCell>{user.firstName}</TableCell>
+                  <TableCell>{user.lastName}</TableCell>
+                  <TableCell>{user.email}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </div>
+    </div>
+  );
 };
--- client/src/components/pages/Login/Login.tsx
+++ client/src/components/pages/Login/Login.tsx
@@ -41,7 +41,7 @@ export const Login = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className="flex items-center justify-center">
+    <form onSubmit={handleSubmit(onSubmit)} className="flex items-center justify-center h-full">
       <div className="w-90">
         <div className="mb-4!">
           <TextField
